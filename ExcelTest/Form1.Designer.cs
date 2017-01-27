5dfd91542ac96e7386e349b67febf9f879f4d1e0
Ryuta Tobita <tobita0112@gmail.com>
2017-01-27 16:48:41 +0900

    first time commit

---
+++ ExcelTest/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ExcelTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateTestData = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreateTestData
+            // 
+            this.btnCreateTestData.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateTestData.Name = "btnCreateTestData";
+            this.btnCreateTestData.Size = new System.Drawing.Size(118, 23);
+            this.btnCreateTestData.TabIndex = 0;
+            this.btnCreateTestData.Text = "テストデータ生成";
+            this.btnCreateTestData.UseVisualStyleBackColor = true;
+            this.btnCreateTestData.Click += new System.EventHandler(this.btnCreateTestData_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(585, 271);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(522, 12);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 2;
+            this.btnOutput.Text = "xlsx出力";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "xlsx";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 324);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCreateTestData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateTestData;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ExcelTest/Form1.cs
@@ -0,0 +1,67 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateTestData_Click(object sender, EventArgs e)
+        {
+            var lst = new List<SelectDataEntity>();
+            for (int i = 1; i <= 10; i++)
+            {
+                lst.Add(new SelectDataEntity(i));
+            }
+            this.dataGridView1.DataSource = lst;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK.Equals(this.saveFileDialog1.ShowDialog()))
+            {
+                var workbook = new XLWorkbook();
+                var worksheet = workbook.Worksheets.Add("Sample Sheet");
+                var lst = (List<SelectDataEntity>)this.dataGridView1.DataSource;
+                
+                worksheet.Cell(1, 1).Value = lst.Select<SelectDataEntity, string>(record => record.ID.ToString());
+                worksheet.Cell(1, 2).Value = lst.Select<SelectDataEntity, string>(record => record.Name);
+                worksheet.Cell(1, 3).Value = lst.Select<SelectDataEntity, string>(record => record.Address);
+                worksheet.Cell(1, 4).Value = lst.Select<SelectDataEntity, string>(record => record.Price.ToString());
+                worksheet.Cell(1, 5).Value = lst.Select<SelectDataEntity, string>(record => record.RegistDate.ToString("yyyy/MM/dd"));
+                workbook.SaveAs(this.saveFileDialog1.FileName);
+
+            }
+        }
+    }
+
+    public class SelectDataEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal Price { get; set; }
+        public DateTime RegistDate { get; set; }
+
+        public SelectDataEntity(int registNumber)
+        {
+            this.ID = registNumber;
+            this.Name = "名前" + registNumber.ToString();
+            this.Address = "住所" + registNumber.ToString();
+            this.Price = registNumber * 100;
+            this.RegistDate = DateTime.Now.AddDays(-registNumber);
+        }
+    }
+}
